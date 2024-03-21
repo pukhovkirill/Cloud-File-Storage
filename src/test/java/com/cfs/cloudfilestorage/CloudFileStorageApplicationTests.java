980b39e6f6aaa20a2801d7171c34f8c62bbe8e95
Pukhov <kirillpuhov526@gmail.com>
2024-03-21 17:22:57 +0300

    init commit

---
+++ src/test/java/com/cfs/cloudfilestorage/CloudFileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfs.cloudfilestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudFileStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
