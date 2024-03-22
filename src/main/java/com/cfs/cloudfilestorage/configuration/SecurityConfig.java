a46521d639bd259edc1b01ad210ac03cc1b7ae88
Pukhov <kirillpuhov526@gmail.com>
2024-03-23 02:00:08 +0300

    refactor(HomeController): split in home and welcome page by authentication

--- src/main/java/com/cfs/cloudfilestorage/configuration/SecurityConfig.java
+++ src/main/java/com/cfs/cloudfilestorage/configuration/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
--- src/main/java/com/cfs/cloudfilestorage/controller/HomeController.java
+++ src/main/java/com/cfs/cloudfilestorage/controller/HomeController.java
@@ -1,12 +1,25 @@
 package com.cfs.cloudfilestorage.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
+    @GetMapping("/")
     public String showHomePage(){
-        return "home";
+        if(isAuthenticated())
+            return "home";
+        return "welcome";
+    }
+
+    private boolean isAuthenticated() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || AnonymousAuthenticationToken.class.
+                isAssignableFrom(authentication.getClass())) {
+            return false;
+        }
+        return authentication.isAuthenticated();
     }
 }
