afa82b320d57f697321e0c30be5e48325d58a699
Pukhov <kirillpuhov526@gmail.com>
2024-03-23 01:47:54 +0300

    feat(service, SecurityConfig): add authorization

--- src/main/java/com/cfs/cloudfilestorage/configuration/SecurityConfig.java
+++ src/main/java/com/cfs/cloudfilestorage/configuration/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.cfs.cloudfilestorage.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +15,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
@@ -29,10 +38,19 @@ public class SecurityConfig {
                 )
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/home")
                         .permitAll()
                 )
                 .rememberMe(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 }
---
+++ src/main/java/com/cfs/cloudfilestorage/service/PersonDetailsService.java
@@ -0,0 +1,41 @@
+package com.cfs.cloudfilestorage.service;
+
+import com.cfs.cloudfilestorage.repository.PersonRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+@Service
+public class PersonDetailsService implements UserDetailsService {
+
+    private final PersonRepository personRepository;
+
+    public PersonDetailsService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var person = personRepository.findByEmail(email);
+
+        if (person != null) {
+            return new org.springframework.security.core.userdetails.User(person.getEmail(),
+                    person.getPassword(),
+                    mapRolesToAuthorities());
+        }else{
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+    }
+
+    private Collection< ? extends GrantedAuthority> mapRolesToAuthorities() {
+        Collection<SimpleGrantedAuthority> role = new HashSet<>();
+        role.add(new SimpleGrantedAuthority("USER"));
+        return role;
+    }
+}
